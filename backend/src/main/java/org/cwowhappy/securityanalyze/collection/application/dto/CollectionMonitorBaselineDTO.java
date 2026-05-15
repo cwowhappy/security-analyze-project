7efe6981ade37d192dd923362ff88460bc65a8f0
cwowhappy <lixiaoyi0611@126.com>
2026-05-15 14:52:50 +0800

    feat(dto): add CollectionMonitorBaselineDTO

---
+++ backend/src/main/java/org/cwowhappy/securityanalyze/collection/application/dto/CollectionMonitorBaselineDTO.java
@@ -0,0 +1,14 @@
+package org.cwowhappy.securityanalyze.collection.application.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * 数据基线 DTO。
+ */
+@Data
+@Builder
+public class CollectionMonitorBaselineDTO {
+
+    private Long totalStocks;
+}
